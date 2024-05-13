9cef820ec5d1a900925c7747ed7621d59eee6279
khaleed2002 <zekomadred159@gmail.com>
2024-05-13 20:49:07 +0300

    react native setup

---
+++ client/App.tsx
@@ -0,0 +1,27 @@
+import Button from '@/components/Button';
+import theme, { Text } from '@/utils/theme';
+import { ThemeProvider } from '@shopify/restyle';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+
+      <View style={styles.container}>
+        <Text variant='text3Xl'>Welcome Developer</Text>
+        <Button />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@/utils/theme'
+import { StyleSheet, Text, View } from 'react-native'
+const Button = () => {
+    return (
+        <Box bg="blu200" p="4" borderRadius="rounded-3xl">
+            <Text>Button / Update</Text>
+        </Box>
+    )
+}
+export default Button
+const styles = StyleSheet.create({})
